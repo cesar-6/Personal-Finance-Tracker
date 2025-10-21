0a93889efd7cae87e75014dc9334dfae7638d8b2
cesar-6 <FuentesHernandez.C173944@stu.sanjac.edu>
2025-10-21 17:13:16 -0500

    Create transaction.py

---
+++ transaction.py
@@ -0,0 +1,8 @@
+class Transaction:
+    def __init__(self, amount, category, date):
+        self.amount = amount
+        self.category = category
+        self.date = date
+
+    def get_summary(self):
+        pass  # Stub method
