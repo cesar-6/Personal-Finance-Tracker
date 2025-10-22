83a81c0c207c69ff1c49d988bc574304621c99cc
cesar-6 <FuentesHernandez.C173944@stu.sanjac.edu>
2025-10-21 20:01:27 -0500

    Create budget.py

---
+++ budget.py
@@ -0,0 +1,7 @@
+class Budget:
+    def __init__(self, category, limit):
+        self.category = category
+        self.limit = limit
+
+    def check_limit(self, amount):
+        pass  # Stub method
