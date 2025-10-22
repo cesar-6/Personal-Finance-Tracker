1d131686adaa5906aea40de258e74d0987fd2ae8
cesar-6 <FuentesHernandez.C173944@stu.sanjac.edu>
2025-10-21 20:03:35 -0500

    Create manager.py

---
+++ manager.py
@@ -0,0 +1,9 @@
+class Manager:
+    def __init__(self):
+        pass  # Stub constructor
+
+    def load_data(self):
+        pass  # Stub method
+
+    def save_data(self):
+        pass  # Stub method
