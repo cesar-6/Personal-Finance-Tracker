22e7c2622d6c1ce552f9364c89218f601144d6bc
cesar-6 <FuentesHernandez.C173944@stu.sanjac.edu>
2025-10-21 20:00:55 -0500

    Create account.py

---
+++ account.py
@@ -0,0 +1,10 @@
+class Account:
+    def __init__(self):
+        self.balance = 0
+        self.transactions = []
+
+    def add_transaction(self, transaction):
+        pass  # Stub method
+
+    def get_balance(self):
+        pass  # Stub method
